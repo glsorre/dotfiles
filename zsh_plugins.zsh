f389d898bfe25beca7e9e07e93dfe12d68f300d3
Giuseppe Sorrentino <uhpnou@gmail.com>
2026-04-04 20:27:43 +0200

    moved from .claude to claude and other improvements

---
+++ zsh_plugins.zsh
@@ -0,0 +1,56 @@
+fpath+=( "$HOME/.cache/antidote/github.com/getantidote/use-omz" )
+source "$HOME/.cache/antidote/github.com/getantidote/use-omz/use-omz.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/async_prompt.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/bzr.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/clipboard.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/cli.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/compfix.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/completion.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/correction.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/diagnostics.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/directories.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/functions.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/git.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/grep.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/history.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/key-bindings.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/misc.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/nvm.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/prompt_info_functions.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/spectrum.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/termsupport.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/theme-and-appearance.zsh"
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/lib/vcs_info.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/starship" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/starship/starship.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/brew" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/brew/brew.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/python" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/python/python.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/pip" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/pip/pip.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/npm" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/npm/npm.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git/git.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/docker-compose" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/docker-compose/docker-compose.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/vscode" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/vscode/vscode.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/fzf" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/fzf/fzf.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/eza" )
+source "$HOME/.cache/antidote/github.com/ohmyzsh/ohmyzsh/plugins/eza/eza.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-completions" )
+source "$HOME/.cache/antidote/github.com/zsh-users/zsh-completions/zsh-completions.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-syntax-highlighting" )
+source "$HOME/.cache/antidote/github.com/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-history-substring-search" )
+source "$HOME/.cache/antidote/github.com/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/djui/alias-tips" )
+source "$HOME/.cache/antidote/github.com/djui/alias-tips/alias-tips.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/caarlos0/zsh-mkc" )
+source "$HOME/.cache/antidote/github.com/caarlos0/zsh-mkc/mkc.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/Tarrasch/zsh-bd" )
+source "$HOME/.cache/antidote/github.com/Tarrasch/zsh-bd/bd.plugin.zsh"
