6790623f1916004367a59fe38038c9a63fb5f81e
Giuseppe Sorrentino <uhpnou@gmail.com>
2026-04-08 22:40:28 +0200

    imrpovements

--- zsh_plugins.zsh
+++ zsh_plugins.zsh
@@ -46,8 +46,6 @@ fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-completions" )
 source "$HOME/.cache/antidote/github.com/zsh-users/zsh-completions/zsh-completions.plugin.zsh"
 fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-syntax-highlighting" )
 source "$HOME/.cache/antidote/github.com/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-history-substring-search" )
-source "$HOME/.cache/antidote/github.com/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
 fpath+=( "$HOME/.cache/antidote/github.com/djui/alias-tips" )
 source "$HOME/.cache/antidote/github.com/djui/alias-tips/alias-tips.plugin.zsh"
 fpath+=( "$HOME/.cache/antidote/github.com/caarlos0/zsh-mkc" )
